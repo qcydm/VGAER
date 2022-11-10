b4c402604b2b2c70799522dd32de41072dbbbd0d
Dragon526 <55922224+Dragon526@users.noreply.github.com>
2022-11-10 15:06:12 +0800

    Update cluster.py

--- VGAER_codes/cluster.py
+++ VGAER_codes/cluster.py
@@ -1,13 +1,15 @@
 from sklearn.cluster import KMeans
 import matplotlib.pyplot as plt
+import pandas as pd
 import numpy as np
-def community (z,clusters):
-    z = z.detach().numpy()
-    C_model = KMeans(n_clusters=clusters, verbose=1, max_iter=100, tol=0.01, n_init=3)
-    C_model.fit(z)
+from sklearn.manifold import TSNE
+#def community (z,clusters):
+    #z = z.detach().numpy()
+    #C_model = KMeans(n_clusters=clusters, verbose=1, max_iter=100, tol=0.01, n_init=3)
+    #C_model.fit(z)
 
     #train label
-    commu_predict = C_model.labels_
+    #commu_predict = C_model.labels_
     # centers = C_model.cluster_centers_
     # # centers_id = []
     # # for i in range(clusters):
@@ -27,4 +29,24 @@ def community (z,clusters):
     # plt.subplot(1, 2, 2)  # 当前绘图区设定为第2格
     # plt.scatter(z[:, 0], z[:, 1], c=commu_predict, marker='o', s=10)  # 不同类别不同颜色
     # plt.title("k-means")
+    #return commu_predict
+def community (z1, clusters):
+    z1 = z1.detach().numpy()
+    #for i in range(1,11):
+    #ts = manifold.TSNE(n_components=2, perplexity=3, early_exaggeration=10, n_iter=50000, learning_rate=500,  angle=0.5, init='random')
+    ts = TSNE(n_components=2, perplexity=50, early_exaggeration=500, n_iter=100000, learning_rate=100, angle=0.5,
+                       init='random')
+    z = ts.fit_transform(z1)
+    #C_model = KMeans(n_clusters=clusters, verbose=0, max_iter=1000, tol=0.001, n_init=20, init='k-means++')
+    C_model = KMeans(n_clusters=clusters, verbose=0, max_iter=1000, tol=0.001, n_init=20, init='k-means++')
+    #C_model = KMeans(n_clusters=clusters, verbose=0, max_iter=100, tol=0.01, n_init=3)
+    C_model.fit(z)
+    commu_predict = C_model.labels_
+    #torch.save(commu_predict, './pred.pt')
+    plt.figure(figsize=(10,10), dpi=80)
+    z = plt.scatter(z[:, 0], z[:, 1], c=commu_predict, marker='o', s=10)  # 不同类别不同颜色
+    plt.title("k-means")
+    #plt.savefig('./cora{}.pdf'.format(i))
+    plt.show()
+    print(i)
     return commu_predict
